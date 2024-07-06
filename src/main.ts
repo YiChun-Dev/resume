924312f03784aed588c3381b5cbf0a74f934cb81
Yi Chun <yichun5927@gmail.com>
2024-07-06 11:01:15 +0800

    feat: add FontAwesome to project
    - Installed FontAwesome library using npm: `npm install --save @fortawesome/fontawesome-free`
    - Imported FontAwesome CSS in `main.ts` to make icons available globally
    - This setup allows us to use a wide range of FontAwesome icons in our Vue components

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// 引入 FontAwesome 的 CSS 文件
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 createApp(App).mount('#app')
