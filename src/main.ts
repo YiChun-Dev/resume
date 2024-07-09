05726257936e1cf014f0fdedd2aab6d6436e8ee8
Yi Chun <yichun5927@gmail.com>
2024-07-09 23:22:05 +0800

    feat: add Bootstrap for project styling
    Added Bootstrap CSS and JavaScript to enhance the project's styling capabilities.
    
    - Imported Bootstrap CSS and JavaScript in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import App from './App.vue'
 
 // 引入 FontAwesome 的 CSS 文件
 import '@fortawesome/fontawesome-free/css/all.min.css'
+// 引入 Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 引入 Bootstrap JavaScript
+import 'bootstrap'
 
 createApp(App).mount('#app')
